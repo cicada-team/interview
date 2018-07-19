97ff803aa81dfea69bddf98bd7262e99be9ed394
ele828 <ele828@gmail.com>
2018-07-19 22:48:37 +0800

    solve 4th

--- 4.js
+++ 4.js
@@ -1,5 +1,7 @@
 const input = [
   [1, 2], [5, 3], [3, 1], [1, 2], [2, 4], [1, 6], [2, 3], [3, 4], [5, 6]
+  // [2,1],[2,3],[1,3],
+  // [1,2],[3,4],[2,3],[4,1]
 ];
 
 function solve(input) {
@@ -7,15 +9,48 @@ function solve(input) {
   for (const cur of input) {
     if (!cache[cur[0]]) cache[cur[0]] = [];
     if (!cache[cur[1]]) cache[cur[1]] = [];
-    console.log(cur[0])
-    console.log(cur[1])
-    cache[cur[0]].push(cur);
-    cache[cur[1]].push(cur.reverse());
+    cache[cur[0]].push(cur.slice());
+    cache[cur[1]].push(cur.slice().reverse());
   }
 
-  console.log(cache);
+  const result = [];
   for (const cur of input) {
-    console.log(cur);
+    const curRes= [];
+    dfs(input, cache, cur, [cur], curRes);
+
+    const curRes2 = [];
+    dfs(input, cache, cur.slice().reverse(), [cur.slice().reverse()], curRes2);
+    result.push(curRes);
+    if (curRes.length > 0) result.concat(curRes);
+    if (curRes2.length > 0) result.concat(curRes2);
+  }
+  return result;
+}
+
+function includes(arr, pair) {
+  return arr.find((a) => a[0] === pair[0] && a[1] === pair[1]);
+}
+
+function dfs(input, cache, pair, sol, result) {
+  if (sol.length === input.length) {
+    if (sol[0][0] === sol[sol.length-1][1]) {
+      const n = [];
+      for (const s of sol) {
+        n.push(s.join(''));
+      }
+      result.push(n);
+    }
+    return;
+  }
+  const next = cache[pair[1]];
+  if (next) {
+    for (const p of next) {
+      // if (!includes(sol, p) && !includes(sol, p.slice().reverse())) {
+        sol.push(p);
+        dfs(input, cache, p, sol, result);
+        sol.pop();
+      // }
+    }
   }
 }
 
